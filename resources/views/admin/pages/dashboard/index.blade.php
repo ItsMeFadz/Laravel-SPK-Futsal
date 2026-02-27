8f989ec72e66069fff463188dc0153a56a6d6752
ItsMeFadz <mr954904@gmail.com>
2026-02-27 18:59:43 +0700

    langkah menggunakan sistem

--- resources/views/admin/pages/dashboard/index.blade.php
+++ resources/views/admin/pages/dashboard/index.blade.php
@@ -14,7 +14,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-12 col-md-6 order-1">
+        <div class="col-lg-12 col-md-6">
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-6 mb-4">
                     <div class="card border-2 border-success shadow-sm">
@@ -74,6 +74,29 @@
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-12 mb-4 order-0">
+            <div class="col-sm-12">
+                <div class="card border-2 border-info text-center">
+                    <div class="card-body">
+                        <h5 class="card-title text-info mt-2">Langkah - Langkah Menggunakan Sistem</h5>
+                        <ol class="text-start d-inline-block">
+                            <li>Tambahkan kriteria pada halaman Kriteria.</li>
+                            <li>Tambahkan data pemain pada halaman Pemain.</li>
+                            <li>Tambahkan data latihan beserta tanggal latihan pada halaman Latihan.</li>
+                            <li>
+                                Pada halaman Penilaian, lakukan penilaian pemain. Penilaian bisa diinput langsung melalui
+                                sistem atau menggunakan format Excel yang dapat diunduh di halaman tersebut. Selain itu,
+                                Anda juga dapat memperbarui status kesehatan pemain pada setiap sesi latihan.
+                            </li>
+                            <li>
+                                Halaman Hasil Penilaian menampilkan rekomendasi line-up utama yang dihitung dari latihan
+                                terakhir, serta menyediakan fitur unduh dalam format Excel dan PDF.
+                            </li>
+                        </ol>
                     </div>
                 </div>
             </div>
